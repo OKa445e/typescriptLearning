c4bb7341341c6681ed738cc531b922b2e7c38cd5
Atharv sinha <atharvdav@gmail.com>
2025-04-23 12:09:38 +0530

    second day of typescript

--- dist/index.js
+++ dist/index.js
@@ -43,8 +43,71 @@ const product = {
     age: 22,
     location: "Bengaluru"
 };
-console.log(product);
 // Never Type - -read this one time
 const errorHandler = () => {
     throw new Error();
 };
+// -----------------------------------------------------------------------
+// classes in Typescript 1:06:10
+/*class Player {
+ private  height;
+    weight;
+
+    constructor(height:number,weight:number) {
+        this.height = height;
+        this.weight = weight;
+    };
+
+
+    myheight = () => {
+      return  this.height;
+    }
+};
+*/
+// another short way
+class Player {
+    constructor(height, weight) {
+        this.height = height;
+        this.weight = weight;
+        this.id = String(Math.random() * 100);
+    }
+    get heightInstance() {
+        return this.height;
+    }
+    set changeHeight(val) {
+        this.height = val;
+    }
+}
+// protected ko sublclass mein access kar sakte hai 
+// while private usi class mein karpayenge
+// class Player2 extends Player {
+//     special: boolean;
+//     constructor(height:number,weight:number,power:number,special:boolean){
+//         super(height,weight,power);
+//         this.special = special;
+//     }
+//     getSpecial = () => {
+//         return this.special;
+//     }
+// }
+const player = new Player(171, 80);
+player.changeHeight = 180;
+class Product {
+    constructor(name, price, stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.id = String(Math.random() * 1000);
+        this.getId = () => this.id;
+    }
+}
+const products = new Product("Iphone", 90000, 120);
+console.log(products.getId());
+// ---------------------------------------------------------------------------------
+// Type assertion
+const btn = document.getElementById('click');
+// const btn = document.getElementById('click')!
+// const btn = document.getElementById('click') as HTMLElement;
+btn.onclick;
+const img = document.getElementById('myimg');
+img.src;
--- src/index.ts
+++ src/index.ts
@@ -79,27 +79,116 @@ const fun3: funcType = (...m) => {
 // function with object
 
 
-interface Product{
+interface Product1{
     name: string,
     age: number,
     location: string
 }
 type getDataType = (product:Product) => void
 
-const product:Product = {
+const product:Product1 = {
     name: "atharva",
     age: 22,
     location: "Bengaluru"
 }
 
-console.log(product);
-
-
 // Never Type - -read this one time
 
 const errorHandler = ():never => {
-    throw new Error();
+    throw new Error(); 
 }
 // -----------------------------------------------------------------------
 
 // classes in Typescript 1:06:10
+
+/*class Player {
+ private  height;
+    weight;
+
+    constructor(height:number,weight:number) {
+        this.height = height;
+        this.weight = weight;
+    };
+
+
+    myheight = () => {
+      return  this.height;
+    }
+};
+*/
+// another short way
+
+class Player {
+   public readonly id:string;
+
+    constructor(
+      private height: number,
+      public weight: number,
+    ){
+    this.id = String(Math.random()*100);
+    }
+
+ get heightInstance():number {
+    return this.height;
+ }
+
+ set changeHeight(val:number) {
+    this.height = val;
+ }
+}
+
+// protected ko sublclass mein access kar sakte hai 
+// while private usi class mein karpayenge
+
+// class Player2 extends Player {
+//     special: boolean;
+
+//     constructor(height:number,weight:number,power:number,special:boolean){
+//         super(height,weight,power);
+//         this.special = special;
+//     }
+//     getSpecial = () => {
+//         return this.special;
+//     }
+// }
+const player = new Player(171,80);
+player.changeHeight = 180;
+// ------------------------------------------------------------------------------
+// Interfaces with classes
+ 
+interface ProductType{
+name: string,
+price: number,
+stock: number,
+offer?:string
+}
+
+interface GiveId{
+getId: () => string
+}
+class Product implements ProductType,GiveId {
+  private id:string = String(Math.random()*1000);
+
+  constructor(
+    public name: string,
+    public price: number,
+    public stock: number
+  ){}
+  getId = () => this.id;
+}
+
+const products = new Product("Iphone",90000,120);
+
+
+console.log(products.getId());
+// ---------------------------------------------------------------------------------
+// Type assertion
+
+const btn = <HTMLElement>document.getElementById('click');
+// const btn = document.getElementById('click')!
+// const btn = document.getElementById('click') as HTMLElement;
+btn.onclick;
+
+const img = document.getElementById('myimg') as HTMLImageElement;
+
+img.src
